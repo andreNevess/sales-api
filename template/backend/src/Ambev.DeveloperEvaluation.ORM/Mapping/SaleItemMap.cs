6dc9eb9259063d7efb0e15653b3fddc8e45ff1c7
André Neves <andre.n.silva@noesis.com.br>
2025-04-15 01:21:24 -0300

    feat(orm): adiciona entidades Sale e SaleItem no DbContext com mapeamento separado

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemMap.cs
@@ -0,0 +1,23 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities.Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class SaleItemMap : IEntityTypeConfiguration<SaleItem>
+    {
+        public void Configure(EntityTypeBuilder<SaleItem> builder)
+        {
+            builder.HasKey(i => i.Id);
+            builder.Property(i => i.ProductId).IsRequired();
+            builder.Property(i => i.ProductName).IsRequired();
+            builder.Property(i => i.Quantity).IsRequired();
+            builder.Property(i => i.UnitPrice).IsRequired();
+        }
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleMap.cs
@@ -0,0 +1,24 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class SaleMap : IEntityTypeConfiguration<Sale>
+    {
+        public void Configure(EntityTypeBuilder<Sale> builder)
+        {
+            builder.HasKey(s => s.Id);
+            builder.Property(s => s.SaleNumber).IsRequired();
+
+            builder.HasMany(s => s.Items)
+                   .WithOne()
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
